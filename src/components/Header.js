7c35ec3076755568972086517bde08f0395f6132
Amir ali <amiralikhamseh01@gmail.com>
2023-10-21 13:25:15 +0200

    CSS optimised and some features added

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <header>
       <div className="container">
         <Link to='/' style={{textDecoration:'none' ,color:'#ff6a95'}}>
-          <h2>Feedback App</h2>
+          <h2>Feedback system</h2>
         </Link>
       </div>
     </header>
